999608e8ab7702526be3363b62b5bb621c2c2d7e
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-20 12:33:24 +0200

    Change export

---
+++ src/redux/Contacts/index.js
@@ -0,0 +1,8 @@
+export { getFilter, getVisibleList } from './selectors';
+export {
+  useFetchContactsQuery,
+  useAddContactMutation,
+  useDeleteContactMutation,
+} from './options';
+
+export { filterContact } from './actions';
