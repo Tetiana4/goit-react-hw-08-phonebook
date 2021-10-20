b4671c0aa4b1d23f4c7d3c8c7e6e7aaac0d10c8e
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-20 16:40:45 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { filterContact } from '../../redux/Contacts';
 import { getFilter } from '../../redux/Contacts';
+import { Label } from './Filter.styled';
 
 export const Filter = () => {
   const value = useSelector(getFilter);
@@ -11,8 +12,10 @@ export const Filter = () => {
   };
 
   return (
-    <label>
+    <Label>
+      <h2>Contacts</h2>
+      <p>Find contacts by name</p>
       <input type="text" value={value} onChange={onChangeChandler} />
-    </label>
+    </Label>
   );
 };
