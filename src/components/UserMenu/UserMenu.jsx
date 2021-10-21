e49c03a8a29fdfada85320771faa52f3f4549021
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-21 10:21:08 +0200

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { authSelectors, authOperations } from '../../redux/auth';
 
 export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.getUserName);
   return (
     <div>
-      <span>Добро пожаловать</span>
-      <button type="button">Выйти</button>
+      <span>Добро пожаловать, {name}</span>
+      <button type="button" onClick={() => dispatch(authOperations.logOut())}>
+        Выйти
+      </button>
     </div>
   );
 };
