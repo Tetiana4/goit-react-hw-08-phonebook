835b3225ea680edb2c623e3600b22087b4d1ccc6
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-21 16:06:29 +0200

    Change styles

--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export const Link = styled(NavLink)`
   display: inline-block;
   text-decoration: none;
-  padding: 12;
+  padding: 12px;
   font-weight: 700;
   color: #2a363b;
   &.active {
