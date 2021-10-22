6925241fa56932728257b59eab5c12889c1cc301
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-22 13:41:12 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,10 @@
 import { Switch, Route } from 'react-router-dom';
-
-import { ContactForm } from '../ContactForm';
 import { HomeView } from '../../views/HomeView/HomeView';
-import { RegisterView } from '../../views/RegisterView/RegisterView';
+import { RegisterView } from '../../views/RegisterView';
 import { LoginView } from '../../views/LoginView/LoginView';
 import { ContactsView } from '../../views/ContactsView';
-import { Filter } from '../Filter';
-import { ContactList } from '../ContactList';
 import { AppBar } from '../AppBar/AppBar';
-import { Container, Title } from './App.styled';
+import { Container } from './App.styled';
 
 export default function App() {
   return (
@@ -21,12 +17,6 @@ export default function App() {
         <Route path="/login" component={LoginView} />
         <Route path="/contacts" component={ContactsView} />
       </Switch>
-
-      <Title>Phonebook</Title>
-      <ContactForm />
-
-      <Filter />
-      <ContactList />
     </Container>
   );
 }
