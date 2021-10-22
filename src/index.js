4bfe8fa804fbe3a3ec77297c9cfef100e26c8fec
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-22 13:41:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import App from './components/App/App';
-// import { PersistGate } from 'redux-persist/integration/react';
+
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
