1863f1163c6bd5422efa82cc205906f7e33e4e01
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-22 11:39:08 +0200

    Update actions.js

--- src/redux/Contacts/actions.js
+++ src/redux/Contacts/actions.js
@@ -1,32 +1,3 @@
 import { createAction } from '@reduxjs/toolkit';
-import axios from 'axios';
 
-import { v4 as uuidv4 } from 'uuid';
 export const filterContact = createAction('changeFilter/Contact');
-
-export const addContact = createAction('add/Contact', data => ({
-  payload: {
-    id: uuidv4(),
-    name: data.name,
-    number: data.number,
-  },
-}));
-
-export const deleteContact = createAction('delete/Contact');
-
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
-
-export async function fetchContacts() {
-  const { data } = await axios.get(`/contacts`);
-  return data;
-}
-
-// export async function addContact(contact) {
-//   const { data } = await axios.post(`/contacts`, contact);
-//   return data;
-// }
-
-// export async function deleteContact(id) {
-//   await axios.delete(`/contacts/${id}`);
-//   return id;
-// }
