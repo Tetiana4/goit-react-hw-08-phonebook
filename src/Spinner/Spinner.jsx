4088c9651892d7c0f1763909b6073d40d6414fbc
Tetiana Shevchuk <76263339+Tetiana4@users.noreply.github.com>
2021-11-01 11:04:24 +0200

    Delete files

--- src/Spinner/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import Loader from 'react-loader-spinner';
-
-export const Spinner = () => {
-  return (
-    <>
-      <Loader type="Puff" color="black" height={50} width={50} timeout={3000} />
-    </>
-  );
-};
