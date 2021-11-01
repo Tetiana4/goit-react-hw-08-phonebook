331f66309bf2194c576b086e09a31a86e93f6b3c
Tetiana Shevchuk <76263339+Tetiana4@users.noreply.github.com>
2021-11-01 11:05:07 +0200

    Update LoginView.jsx

--- src/views/LoginView/LoginView.jsx
+++ src/views/LoginView/LoginView.jsx
@@ -9,14 +9,7 @@ export const LoginView = () => {
   const [password, setPassword] = useState('');
 
   const handleChange = ({ target: { name, value } }) => {
-    switch (name) {
-      case 'email':
-        return setEmail(value);
-      case 'password':
-        return setPassword(value);
-      default:
-        return;
-    }
+    name === 'email' ? setEmail(value) : setPassword(value);
   };
 
   const handleSubmit = e => {
