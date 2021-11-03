dac7a70663c7f16a148008981b4374db270514ba
Tetiana Shevchuk <76263339+Tetiana4@users.noreply.github.com>
2021-11-03 08:29:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './components/App/App';
 
-import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
